473e219e06a243ee9b5460b9bd434d31af20890a
Lucas Spinosa <lucasspinosa.software@gmail.com>
2024-02-06 01:29:37 -0300

    CKEditor5 e barra do menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import * as ClassicEditor from './build/ckeditor';
+import './build/translations/pt-br.js'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +8,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'projeto';
+  public Editor: any = ClassicEditor;
 }
